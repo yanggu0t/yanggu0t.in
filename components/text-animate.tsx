f0f91664f4d7fe18dec08b87b122d79e1aeaf1d4
Gu0TEA1031 <zxc225751@gmail.com>
2024-01-31 18:04:25 +0800

    feat(animation): implement text intro animation with framer motion

---
+++ components/text-animate.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useAnimation, useInView, motion } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+interface RevealProps {
+  children: JSX.Element;
+  className?: string;
+}
+
+const Reveal = ({ children, className }: RevealProps) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
+
+  return (
+    <div ref={ref} className="relative w-fit overflow-hidden">
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        className={cn(
+          "absolute bottom-1 left-0 right-0 top-1 z-20 bg-[#564e63] dark:bg-[#887d9a]",
+          className,
+        )}
+      />
+    </div>
+  );
+};
+
+export default Reveal;
